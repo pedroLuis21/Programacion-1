43c3291576aec2379f9200e1f1117e8ba7cdd087
pedroLuis21 <58313528+pedroLuis21@users.noreply.github.com>
2023-03-03 16:29:36 -0400

    Add files via upload

---
+++ PracticaPOO/PracticaPOO/Garaje.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaPOO
+{
+    public class Garaje : Coche
+    {
+        private Coche auto;
+        public string NombreAveria;
+        public int CochesAtendidos = 0;
+        private bool atenderCoche = true;
+        private bool aceptarCoche=true;
+
+        public Garaje(string marca, string modelo) : base(marca, modelo)
+        {
+        }
+
+        public void AtenderCoche(Coche auto)
+        {
+            if (atenderCoche) { aceptarCoche = true; }
+        }
+
+        public void AceptarCoche (Coche auto, string NombreAveria)
+        {
+            if (aceptarCoche)
+            {
+                CochesAtendidos++;
+                Precio_Averias_ACM =(new Random().NextDouble() * 100);
+                aceptarCoche=false;
+                atenderCoche = false;
+            }
+        }
+
+        public void DevolverCoche()
+        {
+            aceptarCoche = true;
+            atenderCoche = true;
+        }
+
+    }
+}
---
+++ PracticaPOO/PracticaPOO/Motor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaPOO
+{
+    public class Motor
+    {
+        private int litros_de_aceite {get;set;}
+        private int Potencia {get;set;}
+
+        public Motor(int Potencia)
+        {
+            this.litros_de_aceite = 0;
+            this.Potencia = Potencia;
+        }
+
+    }
+}
